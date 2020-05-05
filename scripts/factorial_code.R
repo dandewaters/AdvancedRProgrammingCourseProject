e839f0c0a191eb681e84d55d02283835a0164d03
dandewaters <42219981+dandewaters@users.noreply.github.com>
2020-05-04 23:04:59 -0400

    Finished project

--- scripts/factorial_code.R
+++ scripts/factorial_code.R
@@ -1,6 +1,14 @@
+# ==============================================================
+# File:     Factorial_code.R
+# Author:   Daniel DeWaters
+# Date:     5/4/2020
+# Purpose:  Impliment different methods for computing factorials 
+# ==============================================================
+
 library(purrr)
 library(microbenchmark)
 
+# Compute factorials using a loop
 factorial_loop <- function(n){
   if(n == 0){return(1)}
   else{
@@ -10,18 +18,20 @@ factorial_loop <- function(n){
   }
 }
 
+# Compute factorials using the reduce() function
 factorial_reduce <- function(n){
   if(n == 0){return(1)}
   else{reduce(1:n, function(x,y){x * y})}
 }
 
+# Compute factorials using recursion
 factorial_func <- function(n){
   if(n == 0 | n==1){return(1)}
   else{return(n * factorial_func(n-1))}
 }
 
 
-
+# Compute factorials using memoization
 my_n <- 10
 mem <- c(1, rep(NA, times=my_n-1))
 
@@ -37,11 +47,12 @@ factorial_mem <- function(n){
   }
 }
 
-run_bench <- function(n){
+# Function for running microbenchmark on all factorial methods
+run_microbenchmark <- function(n){
   summary(microbenchmark(factorial_loop(n), 
-                                       factorial_reduce(n),
-                                       factorial_func(n),
-                                       factorial_mem(n)))
+                         factorial_reduce(n),
+                         factorial_func(n),
+                         factorial_mem(n)))
 }
 
-as.data.frame(run_bench(10))
+as.data.frame(run_microbenchmark(10))
--- scripts/oop_code.R
+++ scripts/oop_code.R
@@ -1,33 +1,37 @@
-# Constructor for LongitudinalData Object
-make_LD <- function(df){
-  data <- df %>% nest(-id)
-  structure(data, class("LongitudinalData"))
+# =========================================
+# File:     oop_code.R
+# Author:   Daniel DeWaters
+# Date:     5/4/2020
+# Purpose:  Code for LongitudinalData class
+# =========================================
+
+library(dplyr)
+library(tidyr)
+
+
+# Generic functions for retrieving subject, visit, and room information 
+subject <- function(ld_df, id) UseMethod("subject")
+visit <- function(subject, visit_numb) UseMethod("visit")
+room <- function(visit, room_name) UseMethod("room")
+
+
+
+# LongitudionalData objects methods
+make_LD <- function(df) {
+  ld_df <- df %>% nest(data = c(visit, room, value, timepoint))
+  structure(ld_df, class = c("LongitudinalData"))
 }
 
-## Longitudinal Data methods
-make_LongitudinalData <- function(x){
-  data <- df %>% nest(-id)
-  structure(data, class=c("LongitudinalData"))
-}
-
-print.LongitudinalData <- function(data, id){
+print.LongitudinalData <- function(x) {
   cat("Longitudinal dataset with", length(x[["id"]]), "subjects")
   invisible(x)
 }
 
-subject.LongitudinalData <- function(data, id) {
-  index <- which(data[["id"]] == id)
+subject.LongitudinalData <- function(ld_df, id) {
+  index <- which(ld_df[["id"]] == id)
   if (length(index) == 0){return(NULL)}
-  structure(list(id = id, data = data[["data"]][[index]]),
-            class = "Subject")
+  structure(list(id = id, data = ld_df[["data"]][[index]]), class = "Subject")
 }
-
-
-
-## Functions for extracting subject, visit, and room-specific information
-subject <- function(data, subject_id){UseMethod("subject")}
-visit <- function(data, visit_id){UseMethod("visit")}
-room <- function(visit, room){UseMethod("room")}
 
 
 # Subject object methods
@@ -39,43 +43,46 @@ print.Subject <- function(x) {
 summary.Subject <- function(object) {
   output <- object[["data"]] %>% 
     group_by(visit, room) %>%
-    dplyr::summarise(value = mean(value)) %>% 
+    summarise(value = mean(value)) %>% 
     spread(room, value) %>% 
     as.data.frame
   structure(list(id = object[["id"]],
                  output = output), class = "Summary")
 }
 
-visit.Subject <- function(subject, visit_id) {
-  if (!visit_id %in% 0:2)
-    stop("The visit number must be 0, 1 or 2")
-  data <- subject[["data"]] %>% 
-    filter(visit == visit_id) %>% 
+visit.Subject <- function(subject, visit_num) {
+  if (!visit_num %in% 0:2){stop("The visit number must be 0, 1, or 2")}
+  data <-
+    subject[["data"]] %>% 
+    filter(visit == visit_num) %>% 
     select(-visit)
   structure(list(id = subject[["id"]],
-                 visit_id = visit_id,
+                 visit_num = visit_num,
                  data = data), class = "Visit")
 }
 
 
+
 # Visit object methods
-room.Visit <- function(visit, room) {
-  if (!room %in% visit[["data"]][["room"]])
+room.Visit <- function(visit, room_name) {
+  if (!room_name %in% visit[["data"]][["room"]]){
     stop("Please provide a room name which was part of the visit")
-  data <- visit[["data"]] %>% 
-    filter(room == room) %>% 
+  }
+  data <-
+    visit[["data"]] %>% 
+    filter(room == room_name) %>% 
     select(-room)
   structure(list(id = visit[["id"]],
-                 visit_id = visit[["visit_id"]],
-                 room = room,
+                 visit_num = visit[["visit_num"]],
+                 room = room_name,
                  data = data), class = "Room")
 }
 
 
-## Room object methods
+# Room object methods
 print.Room <- function(x) {
   cat("ID:", x[["id"]], "\n")
-  cat("Visit:", x[["visit_id"]], "\n")
+  cat("Visit:", x[["visit_num"]], "\n")
   cat("Room:", x[["room"]])
   invisible(x)
 }
@@ -87,9 +94,10 @@ summary.Room <- function(object) {
 }
 
 
-## Summary Object methods
+# Summary object method
 print.Summary <- function(x) {
   cat("ID:", x[[1]], "\n")
   print(x[[2]])
   invisible(x)
 }
+
